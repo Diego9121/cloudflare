5dd6da76e2e4467123929fca7746ec66b4299591
Desarrollador <drm.e2104@gmail.com>
2026-05-16 22:59:01 -0400

    revert: back to standard Next.js config for Vercel

--- next.config.js
+++ next.config.js
@@ -17,33 +17,6 @@ const nextConfig = {
   compiler: {
     removeConsole: process.env.NODE_ENV === 'production',
   },
-  webpack: (config, { isServer }) => {
-    if (!isServer) {
-      config.optimization.splitChunks = {
-        chunks: 'all',
-        cacheGroups: {
-          default: false,
-          vendors: false,
-          commons: {
-            name: 'commons',
-            chunks: 'all',
-            minChunks: 2,
-          },
-          lib: {
-            test: /[\\/]node_modules[\\/]/,
-            name(module) {
-              const packageName = module.context.match(
-                /[\\/]node_modules[\\/](.*?)([\\/]|$)/
-              )?.[1];
-              return `npm.${packageName?.replace('@', '')}`;
-            },
-            priority: 10,
-          },
-        },
-      };
-    }
-    return config;
-  },
 };
 
 module.exports = nextConfig;
