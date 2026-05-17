75a822e5a298c22de706772a404604d527641ef5
Desarrollador <drm.e2104@gmail.com>
2026-05-16 22:52:07 -0400

    fix: improve webpack chunking for smaller bundle

--- next.config.js
+++ next.config.js
@@ -17,8 +17,32 @@ const nextConfig = {
   compiler: {
     removeConsole: process.env.NODE_ENV === 'production',
   },
-  outputFileTracingExcludes: {
-    '/**': ['**/node_modules/.cache/**'],
+  webpack: (config, { isServer }) => {
+    if (!isServer) {
+      config.optimization.splitChunks = {
+        chunks: 'all',
+        cacheGroups: {
+          default: false,
+          vendors: false,
+          commons: {
+            name: 'commons',
+            chunks: 'all',
+            minChunks: 2,
+          },
+          lib: {
+            test: /[\\/]node_modules[\\/]/,
+            name(module) {
+              const packageName = module.context.match(
+                /[\\/]node_modules[\\/](.*?)([\\/]|$)/
+              )?.[1];
+              return `npm.${packageName?.replace('@', '')}`;
+            },
+            priority: 10,
+          },
+        },
+      };
+    }
+    return config;
   },
 };
 
